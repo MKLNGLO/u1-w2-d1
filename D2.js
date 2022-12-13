d6aa7135edc1cac8a261edecd4745a630ed37742
MKLNGLO <117443447+MKLNGLO@users.noreply.github.com>
2022-12-13 17:11:52 +0100

    Update D2.js

--- D2.js
+++ D2.js
@@ -1,45 +1,45 @@
-/*
-REGOLE
-- Tutte le risposte devono essere scritte in JavaScript
-- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
-- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
-- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
-- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
-*/
+// /*
+// REGOLE
+// - Tutte le risposte devono essere scritte in JavaScript
+// - Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+// - Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+// - Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+// - Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+// */
 
-/* ESERCIZIO 1
- Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
-*/
+// /* ESERCIZIO 1
+//  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+// */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// /* SCRIVI QUI LA TUA RISPOSTA */
 
-let = "undefined: qualcosa senza valore definito";
-let = "boolean: qualcosa che può essere vero oppure falso";
-let = "number: qualcosa con un valore numerico";
-let = "string: qualcosa con valore testuale";
-let = "symbol: qualcosa che usiamo per identificare qualcosa di unico";
-let =
-  "bigint: qualcosa che usiamo per rappresentare dei numeri interi troppo grandi per JavaScript";
-let =
-  "null: assenza intenzionale di valore in un oggetto, viene trattata come falsa nelle operazioni booleane";
-let =
-  "object: qualcosa a se stante, con esistenza propria e che possiede tipi e proprietà";
-let =
-  "function: un insieme di affermazioni che eseguono un compito o calcolano un valore";
+// let = "undefined: qualcosa senza valore definito";
+// let = "boolean: qualcosa che può essere vero oppure falso";
+// let = "number: qualcosa con un valore numerico";
+// let = "string: qualcosa con valore testuale";
+// let = "symbol: qualcosa che usiamo per identificare qualcosa di unico";
+// let =
+//   "bigint: qualcosa che usiamo per rappresentare dei numeri interi troppo grandi per JavaScript";
+// let =
+//   "null: assenza intenzionale di valore in un oggetto, viene trattata come falsa nelle operazioni booleane";
+// let =
+//   "object: qualcosa a se stante, con esistenza propria e che possiede tipi e proprietà";
+// let =
+//   "function: un insieme di affermazioni che eseguono un compito o calcolano un valore";
 
-/* ESERCIZIO 2
- Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
-*/
+// /* ESERCIZIO 2
+//  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+// */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// /* SCRIVI QUI LA TUA RISPOSTA */
 
-let name = "Michelangelo";
+// let name = "Michelangelo";
 
-let number = 233
+// let number = 233
 
-let number2 = 234
+// let number2 = 234
 
-console.log(number+number2)
+// console.log(number+number2)
 
 // /* ESERCIZIO 3
 //  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
@@ -98,18 +98,18 @@ console.log(number+number2)
 // if (name1 !== name2) {
 //   console.log("Names are different");
 // // }
-let name1 = "Michelangelo"
-let name2 = "Pino"
+// let name1 = "Michelangelo"
+// let name2 = "Pino"
 
 
-console.log(name1.toUpperCase())
+// console.log(name1.toUpperCase())
 
-if (name1 === name2) {
-  console.log("Names are same");
-}
-else if (name2 === "Michelangelo") {
-  console.log("il nome è", name1)
-}
-else {
-  console.log("Names are not the same")
-}
+// if (name1 === name2) {
+//   console.log("Names are same");
+// }
+// else if (name2 === "Michelangelo") {
+//   console.log("il nome è", name1)
+// }
+// else {
+//   console.log("Names are not the same")
+// }
